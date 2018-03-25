486305f9a5d8ddaffe94f28b7beb1c4f6b9dfadd
HanQ <229075284@qq.com>
2018-03-26 00:53:48 +0800

    go on develop

--- yo.js
+++ yo.js
@@ -6,4 +6,4 @@ var env = yeoman.createEnv();
 
 env.register(path.join(__dirname, '../react-framework-yeoman/generators/app'), 'framework');
 
-env.run('framework', function () { console.log(arguments) });
+env.run('framework');
